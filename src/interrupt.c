8bfe01edbdcbe0208192b1aac66197593ea5ed29
sotul04 <suthasoma04@gmail.com>
2024-04-01 19:54:15 +0700

    feat: keyboard interrupt

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,5 +1,9 @@
-#include "header/interrupt/interrupt.h"
+#include "header/cpu/interrupt.h"
 #include "header/cpu/portio.h"
+
+void activate_keyboard_interrupt(void) {
+  out(PIC1_DATA, in(PIC1_DATA) & ~(1 << IRQ_KEYBOARD));
+}
 
 void io_wait(void)
 {
--- src/kernel.c
+++ src/kernel.c
@@ -2,9 +2,9 @@
 #include <stdbool.h>
 #include "header/cpu/gdt.h"
 #include "header/kernel-entrypoint.h"
-#include "header/text/framebuffer.h"
+#include "header/driver/framebuffer.h"
 #include "header/cpu/idt.h"
-#include "header/interrupt/interrupt.h"
+#include "header/cpu/interrupt.h"
 
 void kernel_setup(void)
 {
@@ -18,6 +18,7 @@ void kernel_setup(void)
   load_gdt(&_gdt_gdtr);
   pic_remap();
   initialize_idt();
+  activate_keyboard_interrupt();
   framebuffer_clear();
   framebuffer_set_cursor(0, 0);
   __asm__("int $0x4");
